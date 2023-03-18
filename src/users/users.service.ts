5c851c0fc15b9df4bd36265967e313578856772b
tylerkleeberger <108431027+tylerkleeberger@users.noreply.github.com>
2023-03-18 13:07:40 -0400

    initial

---
+++ src/users/users.service.ts
@@ -0,0 +1,60 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { GetUserArgs } from './dto/args/get-user-args.dto';
+import { CreateUserInput } from './dto/input/create-user-input.dto';
+import { User } from './models/user.model';
+import { UserDocument } from './models/user.schema';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  //create and encrypt password (10 additional keys) -- return secure user to Model from Document
+  async createUser(createUserData: CreateUserInput) {
+    await this.validateCreateUserData(createUserData);
+    const userDocument = await this.usersRepository.create({
+      ...createUserData,
+      password: await bcrypt.hash(createUserData.password, 10),
+    });
+    return this.toModel(userDocument);
+  }
+
+  //make sure email isn't already used
+  private async validateCreateUserData(createUserData: CreateUserInput) {
+    try {
+      await this.usersRepository.findOne({ email: createUserData.email });
+      throw new UnprocessableEntityException('Email already exists.');
+    } catch (err) {}
+  }
+
+  //Find a user
+  async getUser(getUserArgs: GetUserArgs) {
+    const userDocument = await this.usersRepository.findOne(getUserArgs);
+    return this.toModel(userDocument);
+  }
+
+  async validateUser(email: string, password: string) {
+    const userDocument = await this.usersRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(
+      password,
+      userDocument.password,
+    );
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+    return this.toModel(userDocument);
+  }
+
+  //Map the documen to the model (ensures return a model) [don't pass password]
+  private toModel(userDocument: UserDocument): User {
+    return {
+      _id: userDocument._id.toHexString(),
+      email: userDocument.email,
+    };
+  }
+}
